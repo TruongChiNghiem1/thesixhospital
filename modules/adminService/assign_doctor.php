e2649d2b65c9c37952903da280a281e6c23e511a
TruongChiNghiem <nghiem0859774418@gmail.com>
2024-12-12 02:49:09 +0700

    dat dich vu

---
+++ modules/adminService/assign_doctor.php
@@ -0,0 +1,20 @@
+<?php
+include '../../config/connect.php';
+include '../../model/adminService.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $lich_id = $_POST['lich_id'];
+    $doctor_id = $_POST['doctor_id'];
+    $status = $_POST['status'];
+
+    // Cập nhật bác sĩ và trạng thái lịch hẹn
+    $conn = (new connect())->connectDB();
+    $stmt = mysqli_prepare($conn, "UPDATE lich_hen SET id_nhan_vien = ?, trang_thai = ? WHERE id_lich_hen = ?");
+    mysqli_stmt_bind_param($stmt, "iii", $doctor_id, $status, $lich_id);
+    mysqli_stmt_execute($stmt);
+
+    // Chuyển hướng về danh sách lịch
+    header("Location: /thesixhospital/adminIndex.php?m=services&a=list-calendar");
+    exit();
+}
+?>

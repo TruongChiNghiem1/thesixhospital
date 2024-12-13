8aef29ac8a634ba7390d6b5ccf812059ed5b61d4
TruongChiNghiem <nghiem0859774418@gmail.com>
2024-12-13 12:57:05 +0700

    sss

--- config/connect.php
+++ config/connect.php
@@ -10,11 +10,7 @@ class connect
 {
     function connectDB()
     {
-<<<<<<< HEAD
-        $conn = mysqli_connect("localhost", "root", "", "a");
-=======
         $conn = mysqli_connect("localhost", "root", "", "the_six_hospital");
->>>>>>> b0307964c697814fd963a160ccbe708c1a4d2ec9
         return $conn;
     }
 
